09e0cb36c37b915a044bb1747fd9653ee73ea55b
NiharikaNeha <nehaniharikaswain07@gmail.com>
2026-03-28 20:57:25 +0530

    learned operators

---
+++ Operators/script.js
@@ -0,0 +1,19 @@
+// QUES:1
+let num1 = 10;
+let num2 = 3;
+console.log("Addition Of num1 and num2: " , num1 + num2);
+console.log("Modulus Of num1 and num2: " , num1 % num2);
+console.log("Exponential Of num1 and num2: " , num1 ** num2);
+
+// QUES:2 
+let x = 5;
+console.log("Increment of x: " , x++);
+console.log("Decrement of x: " , ++x);
+
+// QUES:3
+let y = 10;
+y += 5;
+y *= 2;
+console.log("The value of y:" , y);
+
+// QUES:4
