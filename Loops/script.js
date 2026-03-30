e05fb5cc809aa795c909c8849791a9b41ea6c3e5
NiharikaNeha <nehaniharikaswain07@gmail.com>
2026-03-30 21:57:55 +0530

    completed question number8

--- Loops/script.js
+++ Loops/script.js
@@ -4,42 +4,42 @@
 // }
 
 // Ques:1 Write a program to print numbers from 1 to 10 using a loop.
-// for (let i = 1; i < 11; i++) {
-//   console.log(i);
-// };
+for (let i = 1; i < 11; i++) {
+  console.log(i);
+};
 
 //Ques:2 Write a program to print numbers from 10 to 1.
-// for (let i = 10; i > 0; i--) {
-//     console.log(i);
-// };
+for (let i = 10; i > 0; i--) {
+    console.log(i);
+};
 
 //Ques:3 Write a program to Print all even numbers from 1 to 20.
-// console.log("Even Number Between 1 to 20:");
-// for (let i = 1; i <= 20; i++) {
-//     if (i % 2 === 0) {
-//         console.log(i); 
-//     }
-// };
+console.log("Even Number Between 1 to 20:");
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 === 0) {
+        console.log(i); 
+    }
+};
 
 //Ques:4 Write a program to Print all odd numbers from 1 to 20.
-// console.log("Odd Number Between 1 to 15:");
-// for (let i = 0; i <= 15; i++) {
-//     if (i % 2 !== 0) {
-//         console.log(i);
-//     };
-// };
+console.log("Odd Number Between 1 to 15:");
+for (let i = 0; i <= 15; i++) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    };
+};
 
 //Ques:5 Write a program to print the table of 5
-// for ( let i = 1; i <= 10; i++){
-//     console.log("5 * " + i + " = " + i * 5);
-// };
+for ( let i = 1; i <= 10; i++){
+    console.log("5 * " + i + " = " + i * 5);
+};
 
 //Ques:6 Write a program to find the sum of numbers from 1 to 100
-// let sum = 0;
-// for (let i = 1; i <= 100; i++){
-//     sum += i;
-// }
-// console.log("The sum of numbes from 1 to 100 is: " + sum);
+let sum = 0;
+for (let i = 1; i <= 100; i++){
+    sum += i;
+}
+console.log("The sum of numbes from 1 to 100 is: " + sum);
 
 //Ques:7 Write a program to find the factorial of a number. 
 let num = 5;
@@ -47,6 +47,8 @@ for (let i = num - 1; i > 0; i--){
     num *= i;
 }
 console.log("The factorial of 5 is: " + num);
+
+//Ques:8 Given a number (e.g., 12345), count how many digits it has using a loop.
 
 //FOR IN LOOP
 // Syntax: for (variable in object) {
