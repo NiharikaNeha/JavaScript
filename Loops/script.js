5074ec06fe744611cb80346f454a33a0bc674685
NiharikaNeha <nehaniharikaswain07@gmail.com>
2026-03-30 10:52:15 +0530

    done ques 1

--- Loops/script.js
+++ Loops/script.js
@@ -2,10 +2,13 @@
 // Syntax: for (initialization; condition; increment/decrement) {
 //   // code to be executed
 // }
+
 // Ques:1 Write a program to print numbers from 1 to 10 using a loop.
 for (let i = 1; i < 11; i++) {
   console.log(i);
 };
+
+//Ques:2
 
 //FOR IN LOOP
 // Syntax: for (variable in object) {
