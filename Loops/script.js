d778be553a9ba78fb3a1e471cfaa36995bfd7928
NiharikaNeha <nehaniharikaswain07@gmail.com>
2026-03-30 19:06:45 +0530

    completed question number4

--- Loops/script.js
+++ Loops/script.js
@@ -4,22 +4,30 @@
 // }
 
 // Ques:1 Write a program to print numbers from 1 to 10 using a loop.
-for (let i = 1; i < 11; i++) {
-  console.log(i);
-};
+// for (let i = 1; i < 11; i++) {
+//   console.log(i);
+// };
 
 //Ques:2 Write a program to print numbers from 10 to 1.
-for (let i = 10; i > 0; i--) {
-    console.log(i);
-};
+// for (let i = 10; i > 0; i--) {
+//     console.log(i);
+// };
 
 //Ques:3 Write a program to Print all even numbers from 1 to 20.
-console.log("Even Number Between 1 to 20:");
-for (let i = 2; i <= 20; i++) {
-    if (i % 2 ===0) {
-        console.log(i); 
-    }
-};
+// console.log("Even Number Between 1 to 20:");
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i); 
+//     }
+// };
+
+//Ques:4 Write a program to Print all odd numbers from 1 to 20.
+// console.log("Odd Number Between 1 to 15:");
+// for (let i = 0; i <= 15; i++) {
+//     if (i % 2 !== 0) {
+//         console.log(i);
+//     };
+// };
 
 //FOR IN LOOP
 // Syntax: for (variable in object) {
