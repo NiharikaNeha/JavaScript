77a300315085dac8d4d1752d8baadbcad070566c
NiharikaNeha <nehaniharikaswain07@gmail.com>
2026-03-30 11:12:36 +0530

    done ques 2,3

--- Loops/script.js
+++ Loops/script.js
@@ -8,7 +8,18 @@ for (let i = 1; i < 11; i++) {
   console.log(i);
 };
 
-//Ques:2
+//Ques:2 Write a program to print numbers from 10 to 1.
+for (let i = 10; i > 0; i--) {
+    console.log(i);
+};
+
+//Ques:3 Write a program to Print all even numbers from 1 to 20.
+console.log("Even Number Between 1 to 20:");
+for (let i = 2; i <= 20; i++) {
+    if (i % 2 ===0) {
+        console.log(i); 
+    }
+};
 
 //FOR IN LOOP
 // Syntax: for (variable in object) {
